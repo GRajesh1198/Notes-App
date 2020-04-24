741e186a5cd87a0ff17320fed6b93593d2bd8991
GRajesh1198 <50822370+GRajesh1198@users.noreply.github.com>
2020-04-24 09:21:18 +0530

    changed folders

---
+++ part3/build/precache-manifest.227282c5a3831eda3726e5083cccaae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df99cd85a8aef1a803dcc76fb3f1cac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b67c422c250233ed6e2",
+    "url": "/static/css/main.871b315b.chunk.css"
+  },
+  {
+    "revision": "9488266d34102f3df6ee",
+    "url": "/static/js/2.411f4275.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.411f4275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b67c422c250233ed6e2",
+    "url": "/static/js/main.d66ba73d.chunk.js"
+  },
+  {
+    "revision": "4caee76f79619d3bb41a",
+    "url": "/static/js/runtime-main.92e7eacc.js"
+  }
+]);
